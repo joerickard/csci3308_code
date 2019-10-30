12393d4603fdba5c30a09ea85cced59a5a80a0c2
joerickard <joe.s.rickard@gmail.com>
2019-10-29 18:01:06 -0600

    post json as python object not string

--- webserver/api.py
+++ webserver/api.py
@@ -1,22 +1,18 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 from flask_restful import Resource, Api
 import json
 
-UPLOAD_FOLDER = '/Upload/'
-
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/api/login", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        print(request.data)
+        username = request.json['username']
+        password = request.json['password']
         return username + ':' + password
-    else:
-        return "Hello Sailor!"
 
 
 if __name__ == "__main__":
