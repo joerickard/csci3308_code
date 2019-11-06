cb14dd8758164039858d3f702cce6ad1320a3ae7
joerickard <joe.s.rickard@gmail.com>
2019-11-05 17:01:16 -0700

    experimenting with sqlalchemy etc

--- webserver/api.py
+++ webserver/api.py
@@ -1,9 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from sqlalchemy import create_engine, MetaData, Table
 import json
+
+engine = create_engine('mysql://root@localhost:3306/SimpleStore', convert_unicode=True)
+metadata = MetaData(bind=engine)
+
+users = Table('users', metadata, autoload=True)
+files = Table('files', metadata, autoload=True)
+permissions = Table('permissions', metadata, autoload=True)
+
+con = engine.connect()
+
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route("/api")
 def index():
@@ -36,11 +48,10 @@ def login():
 def newUser():
     if request.method == 'POST':
         print(request.data)
-        username = request.json['username']
-        password = request.json['password']
-        return {"username": username, "password": password}
+        engine.connect().execute(Table('users', metadata, autoload=True).insert(), name=request.json['username'], password=request.json['password'])
+        return {"status": "recieved"}
     else:
-        return "Must POST to this endpoint to add a user account."
+        return "Must POST to this endpoint to create a user account."
 
 @app.route("/api/deleteUser", methods=['GET', 'POST'])
 def deleteUser():
