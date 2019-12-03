1b97ac90bc017e40ae3c0ad7b6449aeb7acc98bf
Aidenp098 <34198230+Aidenp098@users.noreply.github.com>
2019-12-03 12:05:28 -0700

    added starter jquery to send requests and files
    also put id's on form inputs

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,4 +1,71 @@
+// json_login = {"username": user, "password": password}
 
+// template for sending requests
+/*====================================================*/
+const destination = 'http://127.0.0.1:5000/api/'
+const data={
+    name: "Joe",
+    id: 32
+}
+// button event
+$('.btn').click(function(){
+    // post data to destination
+    $.post(destination, data, function(data, status){
+        console.log(data + 'and status is ' + status);
+    });
+})
+/*=======================================================*/
+
+
+// template for sending files
+/*====================================================*/
+$(document).ready(function() {
+
+    $("#btnSubmit").click(function(event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        // Get form
+        var form = $('#fileUploadForm')[0];
+
+		// Create an FormData object 
+        var data = new FormData(form);
+
+		// If you want to add an extra field for the FormData
+        data.append("CustomField", "This is some extra data, testing");
+
+		// disabled the submit button
+        $("#btnSubmit").prop("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: destination,
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function(data) {
+                $("#result").text(data);
+                console.log("SUCCESS : ", data);
+                $("#btnSubmit").prop("disabled", false);
+            },
+            error: function (e) {
+                $("#result").text(e.responseText);
+                console.log("ERROR : ", e);
+                $("#btnSubmit").prop("disabled", false);
+            }
+        });
+    });
+});
+/*====================================================*/
+
+
+
+
+// some function from template for validation
 (function ($) {
     "use strict";
 
